12eda5b7dbcccdb106bb65fe2cc14fd03f1bb37c
Mapi Pina <mpina09@gmail.com>
2018-03-04 16:57:07 -0500

    functionality almost done. ajax isn't recognizing the name of the actor in the url so it's undefined

--- assets/api.js
+++ assets/api.js
@@ -1,23 +1,22 @@
 // Needs to be active on document load
 $(document).ready(function(){
   var actors = ["Judi Dench", "Diane Lane", "Meryl Streep", "Jennifer Lawrence", "Kate Hudson", "Allison Williams"];
-/*  var actorSelected = actors[i];
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actorSelected + "&api_key=Kit9Fo5OHeGDtHLUurXM65czq5nwC7UU&limit=10"; */
+
 
   //going to focus on pushing new buttons rather than ajax for now
   function createButtons(){
     $("#actor-buttons").empty();
-    
+
     for (var i = 0; i < actors.length; i++) {
       var b = $("<button>");
       b.addClass("actor");
-    b.data("name", actors[i]);
+      b.attr("data-name", actors[i]);
       b.text(actors[i]);
       $("#actor-buttons").append(b);
       
     }
   }
-  //button click event
+  //add actorsbutton click event
   $("#add-actor").click(function(){
     event.preventDefault();
     var actorTyped = $("#actor-input").val().trim();
@@ -25,7 +24,30 @@ $(document).ready(function(){
    createButtons();
   })
 
+  // click on actors names
+  function displayGIFS() {
+    var selected = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Kit9Fo5OHeGDtHLUurXM65czq5nwC7UU&limit=10&tag=" + selected;
+    console.log(queryURL);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      console.log(response);
+      var imageURL = response.data.image_url;
+      var actorImage = $("<img>");
+      actorImage.attr("src", imageURL);
+      $("#actor-gifs").append(actorImage);
+
+    })
+
+  }
+
+
   createButtons();
+  $("#actor-buttons").click(displayGIFS);
+  
 
 
 })
